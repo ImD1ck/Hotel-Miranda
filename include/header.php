7bc1b96eb6b9b8451b1ed3604691cb9badde8d72
ImD1ck <rojasdick12@gamil.com>
2022-08-05 00:52:32 +0200

    Select listo

--- include/header.php
+++ include/header.php
@@ -9,52 +9,39 @@
 
   <link rel="stylesheet" type="text/css" href="./styles/styles.css" />
   <link rel="stylesheet" type="text/css" href="./styles/styles.scss" />
+
+  <script type="text/javascript" src="./js/dropMenu.js"></script>
 </head>
 
 <body>
 <header class="header">
-      <div class="nav__icons">
-        <div class="nav__icon iconCross">
-          <img
-            src="./assets/icons/cross.svg"
-            alt="cross"
-            style="display: none"
-          />
-        </div>
-        <div class="nav__icon iconBars">
-          <img
-            src="./assets/icons/bars-solid.svg"
-            alt="bars"
-            class="nav__icon-bar"
-          />
-        </div>
+  <div class="nav"> 
+    <button class="nav__button" id="buttonNav" onClick="dropdown()"></button>
+  </div>
+    <div class="nav__list" id="dropMenu">
+      <a href="./about.php">About Us</a>
+      <a href="./rooms.php">Rooms</a>
+      <a href="./offers.php">Offers</a>
+      <a href="./contact.php">Contact</a>
+    </div>
+    <div class="title">
+      <div class="logo">
+        <img src="./assets/icons/H.svg" alt="logo" />
       </div>
-      <div class="title">
-        <div class="logo">
-          <img src="./assets/icons/H.svg" alt="logo" />
-        </div>
         <img
           class="logo__name"
           src="./assets/icons/Hotel-Miranda.svg"
           alt="Hotel Miranda"
         />
+    </div>
+    <div class="icons">
+      <div class="icon__item">
+        <img src="./assets/icons/person.svg" alt="logIn" />
       </div>
-      <div class="navigation">
-        <ul class="nav__list">
-          <li class="nav__item"><a href="./about.html">About Us</a></li>
-          <li class="nav__item"><a href="./rooms">Rooms</a></li>
-          <li class="nav__item"><a href="./offers">Offers</a></li>
-          <li class="nav__item"><a href="./contact">Contact</a></li>
-        </ul>
+      <div class="icon__item">
+        <img src="./assets/icons/search.svg" alt="search" />
       </div>
-      <div class="icons">
-        <div class="icon__item">
-          <img src="./assets/icons/person.svg" alt="logIn" />
-        </div>
-        <div class="icon__item">
-          <img src="./assets/icons/search.svg" alt="search" />
-        </div>
-      </div>
+    </div>
 
     </header>
 
@@ -64,16 +51,15 @@
         <h1 class="principal__title">
         <?php echo $title; ?>
         </h1>
-      </article>
 
       <?php if($page == ""): ?>
-      <article class="principal">
         <div class="button__principal">
           <button class="button">TAKE A TOUR</button>
           <button class="button">LEARN MORE</button>
         </div>
-      </article>
       <?php else:  ?>
+      </article>
+
       <article class="subtitle">
         <h4>Home | <span>Room Details</span></h4>
       </article>
---
+++ js/dropMenu.js
@@ -0,0 +1,11 @@
+function dropdown() {
+  // let cross = "./assets/icons/cross.svg";
+  // let bar = "./assets/icons/bars-solid.svg";
+  // let img = document.getElementById("menuImg").src;
+  document.getElementById("dropMenu").classList.toggle("nav__show");
+  document.getElementById("buttonNav").classList.toggle("nav__button2");
+
+  // document.getElementById("menuImg").src !== bar
+  //   ? (document.getElementById("menuImg").src = cross)
+  //   : (document.getElementById("menuImg").src = bar);
+}
